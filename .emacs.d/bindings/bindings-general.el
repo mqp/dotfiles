34691d9280b1ab0d44c09a175c047e867cefca27
maugrim <marshall@quander.me>
2013-09-04 14:38:13 -0700

    Vast sweeping emacs changes

--- .emacs.d/bindings/bindings-general.el
+++ .emacs.d/bindings/bindings-general.el
@@ -5,9 +5,20 @@
       (message "Opening file...")
     (message "Aborting")))
 
-(global-set-key (kbd "C-x f") 'ido-recentf-open)
-
+(global-set-key (kbd "C-h a") 'apropos)
 (global-set-key (kbd "M-f") 'forward-word)
 (global-set-key (kbd "M-b") 'backward-word)
-
+(global-set-key (kbd "M-/") 'hippie-expand)
+(global-set-key (kbd "C-c q") 'join-line)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+(global-set-key (kbd "C-x f") 'ido-recentf-open)
+(global-set-key (kbd "C-x C-r") 'rgrep)
+(global-set-key (kbd "C-+") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "C-M-s") 'isearch-forward)
+(global-set-key (kbd "C-M-r") 'isearch-backward)
 (global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,20 +1,11 @@
-;; Turn off mouse interface early in startup to avoid momentary
-;; display
-
+;; turn off mouse interface early in startup to avoid momentary display
 (menu-bar-mode -1)
 (tool-bar-mode -1)
-(setq recentf-save-file "~/.emacs.d/.recentf")
-(recentf-mode 1)
-(setq echo-keystrokes 0.01)
-(setq visible-bell nil)
-(setq message-log-max t)
-(set-frame-font "Consolas-13")
+(scroll-bar-mode -1)
 
-;; Don't clutter up directories with backup files~
-(setq backup-directory-alist `(("." . "~/.emacs.d/backups")))
-
-;; ...or #autosave files
-(setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
+;; work around a bug where system-name is FQDN on OS X
+(if (eq system-type 'darwin)
+    (setq system-name (car (split-string system-name "\\."))))
 
 (setq frame-title-format
       '(:eval
@@ -22,18 +13,114 @@
             (replace-regexp-in-string
              (concat "/home/" user-login-name) "~" buffer-file-name)
           "%b")))
+(set-frame-font "Consolas-13")
 
-(setq initial-scratch-message nil)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+(setq
+ echo-keystrokes 0.1
+ message-log-max t
+ mouse-wheel-mode t
+ xterm-mouse-mode t
+ color-theme-is-global t
+ delete-by-moving-to-trash t
+ font-lock-maximum-decoration t
+ blink-cursor-mode -1
+ visible-bell nil
+ inhibit-startup-screen t
+ initial-scratch-message nil
+ x-select-enable-clipboard t
+ x-select-enable-primary t
+ save-interprogram-paste-before-kill t
+ apropos-do-all t
+ mouse-yank-at-point t
+ custom-file (concat user-emacs-directory "custom.el")
+ package-user-dir (concat user-emacs-directory "elpa")
+ backup-directory-alist `(("." . ,(concat user-emacs-directory
+                                          "backups")))
+ auto-save-file-name-transforms `((".*" ,temporary-file-directory t))
+ auto-fill-mode -1
+ indent-tabs-mode nil
+ tab-width 8
+ global-auto-revert-mode t)
 
 (add-to-list 'backup-directory-alist (cons tramp-file-name-regexp nil))
+
+;; hippie-expand: at times perhaps too hip
+(delete 'try-expand-line hippie-expand-try-functions-list)
+(delete 'try-expand-list hippie-expand-try-functions-list)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(add-hook 'prog-mode-hook 'linum-mode)
+(add-hook 'prog-mode-hook 'electric-pair-mode)
+(add-hook 'prog-mode-hook 'electric-indent-mode)
+(add-hook 'prog-mode-hook
+          '(lambda () (local-set-key (kbd "RET") 'newline-and-indent)))
+
+(defun set-background-process ()
+  "Sets the current buffer process to not pop up an annoying notification on Emacs exit."
+  (set-process-query-on-exit-flag (get-buffer-process (current-buffer)) nil))
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(add-hook 'comint-exec-hook 'set-background-process)
+
+(require 'ido)
+(setq-default
+ ido-max-directory-size 100000
+ ido-max-prospects 10
+ ido-enable-flex-matching t
+ ido-enable-prefix nil
+ ido-save-directory-list-file (concat user-emacs-directory "ido.hist"))
+(ido-mode t)
+
+(require 'uniquify)
+(setq-default
+ uniquify-buffer-name-style 'forward
+ uniquify-ignore-buffers-re "^\\*")
+
+(require 'saveplace)
+(setq-default
+  save-place-file (concat user-emacs-directory "places")
+  save-place t)
+
+(require 'recentf)
+(setq-default
+ recentf-max-saved-items 500
+ recentf-max-menu-items 15)
+ recentf-save-file (concat user-emacs-directory "recentf")
+(recentf-mode 1)
+
+;; savehist keeps track of some history
+(require 'savehist)
+(setq-default
+ savehist-additional-variables '(search ring regexp-search-ring)
+ savehist-autosave-interval 60)
+ savehist-file (concat user-emacs-directory "savehist")
+(savehist-mode 1)
+
+(require 'paren)
+(setq-default
+ show-paren-style 'parenthesis)
+(show-paren-mode 1)
+
+(add-to-list 'auto-mode-alist '("\\.zsh\\'" . shell-script-mode))
+
+;; Emacs Lisp
+(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'lisp-interaction-mode-hook 'pretty-lambdas)
+(add-hook 'emacs-lisp-mode-hook 'auto-complete-mode)
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'emacs-lisp-mode-hook 'pretty-lambdas)
 
 ;; elpa packages
 (require 'package)
 (setq package-archives
-      '(("gnu" . "http://elpa.gnu.org/packages/")
-        ("marmalade" . "http://marmalade-repo.org/packages/")
-	("melpa" . "http://melpa.milkbox.net/packages/")))
-
+      '(("melpa" . "http://melpa.milkbox.net/packages/")
+	("marmalade" . "http://marmalade-repo.org/packages/")
+	("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
 
 (when (not package-archive-contents)
@@ -46,6 +133,7 @@
     popup
     fuzzy
     flymake-cursor
+    ido-ubiquitous
     zenburn-theme
     slime
     slime-repl
@@ -54,6 +142,7 @@
     markdown-mode
     nginx-mode
     nrepl
+    smex
     csharp-mode
     php-mode
     haskell-mode)
@@ -76,33 +165,61 @@
 
 (auto-compile-on-save-mode 1)
 (auto-compile-on-load-mode 1)
+(ido-ubiquitous-mode 1)
+
+;; smex
+(require 'smex)
+(setq-default smex-save-file (concat user-emacs-directory "smex-items"))
+(smex-initialize)
 
 (require 'auto-complete-config)
 (ac-config-default)
+(setq-default
+ ac-auto-start nil
+ ac-auto-show-menu nil
+ ac-use-quick-help t
+ ac-use-menu-map t
+ ac-quick-help-delay 0.2)
 (ac-set-trigger-key "TAB")
 (define-key ac-completing-map "\M-/" 'ac-stop)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
-(setq ac-auto-start nil)
-(setq ac-auto-show-menu nil)
-(setq ac-use-quick-help t)
-(setq ac-use-menu-map t)
-(setq ac-quick-help-delay 0.2)
 
 ;; hook AC into completion-at-point
 (defun set-auto-complete-as-completion-at-point-function ()
   (setq completion-at-point-functions '(auto-complete)))
 (add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
 
-(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
-(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
-(add-hook 'emacs-lisp-mode-hook 'auto-complete-mode)
+(defun revert-all-buffers ()
+  "Refreshes all open buffers from their respective files."
+  (interactive)
+  (dolist (buf (buffer-list))
+    (with-current-buffer buf
+      (when (and (buffer-file-name) (not (buffer-modified-p)))
+        (revert-buffer t t t))))
+  (message "Refreshed open files."))
+
+(defun kill-other-buffers ()
+  "Kill all buffers except the current buffer."
+  (interactive)
+  (mapc 'kill-buffer (delq (current-buffer) (buffer-list))))
+
+(defun pretty-lambdas ()
+  (font-lock-add-keywords
+   nil `(("(\\(lambda\\>\\)"
+          (0 (progn (compose-region (match-beginning 1) (match-end 1)
+                                    ,(make-char 'greek-iso8859-7 107))
+                    nil))))))
 
 (defun pretty-fn ()
-  (font-lock-add-keywords nil `(("(\\(\\<fn\\>\\)"
-                                 (0 (progn (compose-region (match-beginning 1)
-                                                           (match-end 1)
-                                                           "\u0192"
-                                                           'decompose-region)))))))
+  (font-lock-add-keywords
+   nil `(("(\\(\\<fn\\>\\)"
+	  (0 (progn (compose-region (match-beginning 1) (match-end 1)
+				    "\u0192"
+				    'decompose-region)))))))
+
+;; Clojure
+(add-hook 'clojure-mode-hook 'auto-complete-mode)
+(add-hook 'clojure-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'clojure-mode-hook 'pretty-fn)
 (add-hook 'clojure-mode-hook 'subword-mode)
 
@@ -113,6 +230,7 @@
      (put-clojure-indent 'run 'defun)
      (put-clojure-indent 'fresh 'defun)))
 
+;; nREPL
 (add-hook 'nrepl-mode-hook 'subword-mode)
 (add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
 (setq nrepl-history-file "~/.emacs.d/nrepl-history")
@@ -131,38 +249,39 @@
      (define-key nrepl-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
      (define-key nrepl-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)))
 
+;; Extempore
 (autoload 'extempore-mode "~/.emacs.d/vendor/extempore-mode/extempore.el" "" t)
 (add-to-list 'auto-mode-alist '("\\.xtm$" . extempore-mode))
 
+;; HTML
 (add-to-list 'auto-mode-alist '("\\.aspx$'" . html-mode))
 (add-to-list 'auto-mode-alist '("\\.tmpl$'" . html-mode))
 (add-to-list 'auto-mode-alist '("\\.jt$'" . html-mode))
+
+;; Markdown
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+
+;; C#
 (add-to-list 'auto-mode-alist '("\\.cs$" . csharp-mode))
+
+;; PHP
 (add-to-list 'auto-mode-alist '("\\.\\(php\\|inc\\)$" . php-mode))
 
-;; javascript
-(add-hook 'js-mode-hook (lambda () (flymake-mode 1)))
-(add-hook 'js-mode-hook 'subword-mode)
-
+;; Javascript
 (autoload 'fn-mode "~/.emacs.d/vendor/fn-mode/fn-mode.el" t)
 (add-hook 'js-mode-hook 'fn-mode)
+(add-hook 'js-mode-hook 'flymake-mode)
+(add-hook 'js-mode-hook 'subword-mode)
 
-(setq auto-fill-mode -1)
-(setq ido-max-directory-size 100000)
-(add-hook 'prog-mode-hook 'linum-mode)
-(add-hook 'prog-mode-hook 'electric-pair-mode)
-(add-hook 'prog-mode-hook 'electric-indent-mode)
-
-;; python
+;; Python
 (setq py-install-directory "~/.emacs.d/vendor/python-mode/")
 (setq py-shell-name "ipython")
 
-;; haskell
+;; Haskell
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 
-;; erc
+;; ERC
 (add-hook 'erc-mode-hook (lambda () (auto-fill-mode 0)))
 (make-variable-buffer-local 'erc-fill-column)
 (add-hook 'window-configuration-change-hook
@@ -192,32 +311,6 @@
 (defun erc-connect ()
   "Connect to IRC."
   (interactive)
-  (erc :server "irc.freenode.net" :port 6667 :nick "cata")
-  ;;(erc-tls :server "ircd.quixey.com" :port 6697 :nick "mquander" :password "focus")
-  )
+  (erc :server "irc.freenode.net" :port 6667 :nick "cata"))
 
 (load-file "~/.emacs.d/bindings/bindings-general.elc")
-(add-hook 'prog-mode-hook
-          '(lambda () (local-set-key (kbd "RET") 'newline-and-indent)))
-
-(setq-default indent-tabs-mode nil)
-
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
-
-(defadvice save-buffers-kill-emacs (around no-query-kill-emacs activate)
-  "Prevent annoying \"Active processes exist\" query when you quit Emacs."
-  (cl-flet ((process-list)) ad-do-it))
-
-(defun revert-all-buffers ()
-  "Refreshes all open buffers from their respective files."
-  (interactive)
-  (dolist (buf (buffer-list))
-    (with-current-buffer buf
-      (when (and (buffer-file-name) (not (buffer-modified-p)))
-        (revert-buffer t t t))))
-  (message "Refreshed open files."))
-
-(defun kill-other-buffers ()
-  "Kill all buffers except the current buffer."
-  (interactive)
-  (mapc 'kill-buffer (delq (current-buffer) (buffer-list))))
