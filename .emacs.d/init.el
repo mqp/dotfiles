89ee75fd80b85a62dbe2f68a11488e9da73b4c13
maugrim <marshall@quander.me>
2013-09-04 11:43:30 -0700

    Add auto-complete package

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -32,16 +32,16 @@
 (setq package-archives
       '(("gnu" . "http://elpa.gnu.org/packages/")
         ("marmalade" . "http://marmalade-repo.org/packages/")
-        ("melpa" . "http://melpa.milkbox.net/packages/")))
+	("melpa" . "http://melpa.milkbox.net/packages/")))
+
 (package-initialize)
 
 (when (not package-archive-contents)
   (package-refresh-contents))
 
 (defvar my-packages
-  '(starter-kit
-    starter-kit-bindings
-    auto-complete
+  '(auto-complete
+    auto-compile
     ac-nrepl
     popup
     fuzzy
