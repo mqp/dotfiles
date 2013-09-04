cfeb28f82ce4853057512226ac38baf82e0c0520
maugrim <marshall@quander.me>
2013-09-04 11:43:43 -0700

    Change flet to cl-flet

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -206,7 +206,7 @@
 
 (defadvice save-buffers-kill-emacs (around no-query-kill-emacs activate)
   "Prevent annoying \"Active processes exist\" query when you quit Emacs."
-  (flet ((process-list)) ad-do-it))
+  (cl-flet ((process-list)) ad-do-it))
 
 (defun revert-all-buffers ()
   "Refreshes all open buffers from their respective files."
