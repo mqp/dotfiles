fb1bb78ad1c4caa9d7f0ff8ee7843d9b89eb15a5
maugrim <marshall@quander.me>
2015-11-12 02:02:17 -0800

    Dotfiles now work with stow

---
+++ README.md
@@ -0,0 +1,7 @@
+# Dotfiles
+
+Personal dotfiles for my Linux environment. Managed using [GNU Stow][stow]; see e.g.
+[this summary](http://brandon.invergo.net/news/2012-05-26-using-gnu-stow-to-manage-your-dotfiles.html)
+for how to apply them.
+
+[stow]: https://www.gnu.org/software/stow/
--- clojure/.lein/profiles.clj
+++ clojure/.lein/profiles.clj
@@ -1,6 +1,8 @@
 ;; ~/.lein/profiles.clj
 {:user {:plugins [[lein-pprint "1.1.1"]
-                  [cider/cider-nrepl "0.7.0-SNAPSHOT"]]
+                  [cider/cider-nrepl "0.10.0-SNAPSHOT"]
+                  [clojurewerkz/elastisch "2.1.0"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.10"]]
        :injections [(use 'clojure.repl)
                     (use 'clojure.java.javadoc)
                     (use 'clojure.pprint)] }}
