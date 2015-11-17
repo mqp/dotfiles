8a92b1a871eca7883d04bede03fbedc2ca267ed6
maugrim <marshall@quander.me>
2015-11-16 17:14:29 -0800

    Add lein shell script, update nREPL version

--- clojure/.lein/profiles.clj
+++ clojure/.lein/profiles.clj
@@ -2,7 +2,7 @@
 {:user {:plugins [[lein-pprint "1.1.1"]
                   [cider/cider-nrepl "0.10.0-SNAPSHOT"]
                   [clojurewerkz/elastisch "2.1.0"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.10"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]
        :injections [(use 'clojure.repl)
                     (use 'clojure.java.javadoc)
                     (use 'clojure.pprint)] }}
