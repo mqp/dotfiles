df085ae9182cc41722cf526934335499a9b89561
Marshall Polaris <marshall@pol.rs>
2022-06-23 17:24:59 -0700

    Tree sitter stuff (this is sweet!!!)

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -197,14 +197,51 @@
 (add-to-list 'auto-mode-alist '("firestore\\.rules$" . fsr-mode))
 
 (use-package typescript-mode
+  :after tree-sitter
   :mode "\\.\\(ts\\|tsx\\)$"
   :config
+  (define-derived-mode typescript-tsx-mode typescript-mode "TSX"
+    "Major mode for editing TSX files.")
+  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-tsx-mode))
+  (tree-sitter-require 'tsx)
+  (add-to-list 'tree-sitter-major-mode-language-alist '(typescript-tsx-mode . tsx))
   (add-hook 'typescript-mode-hook 'lsp)
   (setq-default typescript-indent-level 2))
 
 (use-package amx
   :config
   (amx-mode 1))
+
+(use-package tree-sitter
+  :ensure t
+  :config
+  ;; activate tree-sitter on any buffer containing code for which it has a parser available
+  (global-tree-sitter-mode)
+  ;; you can easily see the difference tree-sitter-hl-mode makes for python, ts or tsx
+  ;; by switching on and off
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+
+(use-package tree-sitter-langs
+  :ensure t
+  :after tree-sitter)
+
+(use-package quelpa)
+(quelpa
+ '(quelpa-use-package
+   :fetcher git
+   :url "https://github.com/quelpa/quelpa-use-package.git"))
+(require 'quelpa-use-package)
+
+(use-package tsi
+  :after tree-sitter
+  :quelpa (tsi :fetcher github :repo "orzechowskid/tsi.el")
+  ;; define autoload definitions which when actually invoked will cause package to be loaded
+  :commands (tsi-typescript-mode tsi-json-mode tsi-css-mode)
+  :init
+  (add-hook 'typescript-mode-hook (lambda () (tsi-typescript-mode 1)))
+  (add-hook 'json-mode-hook (lambda () (tsi-json-mode 1)))
+  (add-hook 'css-mode-hook (lambda () (tsi-css-mode 1)))
+  (add-hook 'scss-mode-hook (lambda () (tsi-scss-mode 1))))
 
 (use-package lsp-mode
   :commands lsp
